b996680c33bb0780fedefa427bae7bc129813649
Hayagreev <hayagreev.chintu1@gmail.com>
2021-07-03 15:11:47 +0530

    solved reverse only letters

---
+++ 917_reverseOnlyLetters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        i = 0
+        j = len(s) - 1
+        s = list(s)
+        while i < j:
+            if not s[i].isalpha():
+                i += 1
+                continue
+            if not s[j].isalpha():
+                j -= 1
+                continue
+            s[i] , s[j] = s[j] , s[i]
+            i += 1
+            j -= 1
+        return "".join(s)
