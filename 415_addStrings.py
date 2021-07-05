e26578b5f8c3bcf0d430878d0caa93da5fd8f27a
Hayagreev <hayagreev.chintu1@gmail.com>
2021-07-05 16:19:33 +0530

    solved add strings

---
+++ 415_addStrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        int_num1 = 0
+        int_num2 = 0
+        for i in num1:
+            int_num1 = int_num1 * 10 + int(i)
+        for i in num2:
+            int_num2 = int_num2 * 10 + int(i)
+        return str(int_num1 + int_num2)
