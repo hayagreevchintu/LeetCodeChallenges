3d803b2a20132373bc5a47f64cb7075f0941ebc1
Hayagreev <hayagreev.chintu1@gmail.com>
2021-07-05 16:21:39 +0530

    solved add strings

--- 415_addStrings.py
+++ 415_addStrings.py
@@ -3,7 +3,7 @@ class Solution:
         int_num1 = 0
         int_num2 = 0
         for i in num1:
-            int_num1 = int_num1 * 10 + int(i)
+            int_num1 = int_num1 * 10 + (ord(i) - ord('0'))
         for i in num2:
-            int_num2 = int_num2 * 10 + int(i)
+            int_num2 = int_num2 * 10 + (ord(i) - ord('0'))
         return str(int_num1 + int_num2)
